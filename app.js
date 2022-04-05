0f3ac3411ebb59c59371db7b8e333d2092f4fd04
11050 <wstudent898@gmail.com>
2022-04-05 12:18:40 +0300

    notes.json

---
+++ app.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const app = express()
+
+const fs = require('fs')
+app.set('view engine', 'pug')
+app.use('/static', express.static('public'))
+app.use(express.urlencoded( { extended: false }))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+app.get('/create', (req, res) => {
+    res.render('create')
+})
+
+app.post('/create', (req, res) => {
+     const title =req.body.title
+     const description =req.body.description
+    if (title.trim() === '' && description.trim()  === ''){
+        res.render('create', { error: true })
+    } else {
+        fs.readFile('./data/notes.json', (err, data) => {
+           if (err) throw err
+           const notes = JSON.parse(data)
+
+           notes.push({
+               id: id(),
+               title: title,
+               description: description,
+               
+           })
+           fs.writeFile('./data/notes.json', JSON.stringify(notes), err => {
+            if (err) throw err
+
+               res.render('create', {success: true})
+           })
+        })
+    }
+})
+
+
+app.get('/notes', (req, res) => {
+    fs.readFile('./data/notes.json',(err, data) => {
+        if (err) throw err
+        const notes = JSON.parse(data)
+        res.render('notes', { notes: notes })
+    })
+    
+})
+app.get('/notes/:id', (req, res) => {
+    const id = req.params.id
+    
+    fs.readFile('./data/notes.json',(err, data) => {
+        if (err) throw err
+        const notes = JSON.parse(data)
+        const note = notes.filter(note => note.id == id)[0]
+        res.render('detail', { note: note })
+    })
+    
+})
+app.get('/:id/delete', (req, res) => {
+    const id = req.params.id
+
+    fs.readFile('./data/notes.json',(err,data) => {
+        if (err) throw err
+
+        const notes = JSON.parse(data)
+        const filterednotes = notes.filter(note => note.id != id)
+        fs.writeFile('./data/notes.json', JSON.stringify(filterednotes), (err) => {
+            if (err) throw err
+            res.render('home',{ notes: filterednotes, delete: true })
+        })
+         
+
+    })
+ })
+app.listen(8000, err => {
+    if (err) console.log(err)
+
+    console.log('Server is runnign on port 8000')
+ })
+ function id () {
+
+    return '_' + Math.random().toString(36).substr(2,8);
+ }
